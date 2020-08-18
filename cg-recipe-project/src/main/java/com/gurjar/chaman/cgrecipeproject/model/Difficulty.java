9822e9977b1335560180a8407bcd38cf7a0a54bd
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-18 18:28:02 +0530

    Difficulty Enum added.

---
+++ cg-recipe-project/src/main/java/com/gurjar/chaman/cgrecipeproject/model/Difficulty.java
@@ -0,0 +1,10 @@
+package com.gurjar.chaman.cgrecipeproject.model;
+
+/**
+ * @author - Chaman Gurjar
+ * @version - 1.0.0 - 18-Aug-2020
+ */
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
