e65d8c3b44b56f9fd2d23327b379f05c7456d17c
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-18 09:31:13 +0530

    Recipe Porject Created.

---
+++ cg-recipe-project/src/main/java/com/gurjar/chaman/cgrecipeproject/CgRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gurjar.chaman.cgrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgRecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgRecipeProjectApplication.class, args);
+	}
+
+}
