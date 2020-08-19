8e28bdc53044908b61fd75d162c156f6c38eb80b
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-19 08:13:15 +0530

    Code Refactoring and Bi Directional mapping related fixes

--- cg-recipe-project/src/main/java/com/gurjar/chaman/cgrecipeproject/model/Recipe.java
+++ cg-recipe-project/src/main/java/com/gurjar/chaman/cgrecipeproject/model/Recipe.java
@@ -44,12 +44,6 @@ public class Recipe {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Recipe addIngredient(Ingredient ingredient){
-        ingredient.setRecipe(this);
-        this.ingredients.add(ingredient);
-        return this;
-    }
-
     public Long getId() {
         return id;
     }
@@ -128,6 +122,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public Set<Ingredient> getIngredients() {
