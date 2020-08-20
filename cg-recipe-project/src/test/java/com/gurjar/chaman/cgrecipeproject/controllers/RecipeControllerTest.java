53a1d043020a7e8696ca919533a85741923f821f
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-20 19:35:58 +0530

    Mockito Argument Capture

--- cg-recipe-project/src/test/java/com/gurjar/chaman/cgrecipeproject/controllers/RecipeControllerTest.java
+++ cg-recipe-project/src/test/java/com/gurjar/chaman/cgrecipeproject/controllers/RecipeControllerTest.java
@@ -1,15 +1,22 @@
 package com.gurjar.chaman.cgrecipeproject.controllers;
 
+import com.gurjar.chaman.cgrecipeproject.model.Recipe;
 import com.gurjar.chaman.cgrecipeproject.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 /**
  * @author - Chaman Gurjar
@@ -35,9 +42,27 @@ class RecipeControllerTest {
 
     @Test
     void getAllRecipes() {
+
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipeSet.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>>  argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String allRecipesViewName = recipeController.getAllRecipes(model);
+
+        //then
         assertEquals("recipe", allRecipesViewName);
         Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
-        Mockito.verify(model, Mockito.times(1)).addAttribute(eq("recipes"), anySet());
+        Mockito.verify(model, Mockito.times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> value = argumentCaptor.getValue();
+        assertEquals(2, value.size());
     }
 }
